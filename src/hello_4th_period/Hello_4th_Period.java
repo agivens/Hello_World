9de4582a5b0743a44bdce0d6a99fb8018bbe31db
Alethea Givens <agivens2082@stu.neisd.net>
2015-09-15 07:42:44 -0500

    This is my 1st Netbeans project. Hello World with a twist.

---
+++ src/hello_4th_period/Hello_4th_Period.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello_4th_period;
+
+/**
+ *
+ * @author agivens
+ */
+public class Hello_4th_Period {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 20; i > 0; i--) {
+            System.out.println("Hello, in " + i + "!");
+        }
+
+    }
+
+}
